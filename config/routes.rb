4891a660fb740226cf4665408433c6921aff2323
joshweinstock <jyweinstock@uchicago.edu>
2025-03-04 21:04:03 +0000

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "regulations#index"
+
 
   # Routes for the Regulation agency resource:
 
@@ -61,7 +63,7 @@ Rails.application.routes.draw do
   # Routes for the Regulation resource:
 
   # CREATE
-  post("/", { :controller => "regulations", :action => "create" })
+  post("/insert_regulation", { :controller => "regulations", :action => "create" })
           
   # READ
   get("/regulations", { :controller => "regulations", :action => "index" })
